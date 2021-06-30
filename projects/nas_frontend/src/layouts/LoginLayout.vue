6c35271906906b3f570337455209e078b5908081
Jibao Mansaray <jibaomansaray@gmail.com>
2021-06-30 12:45:56 +0000

    Add login layout

---
+++ projects/nas_frontend/src/layouts/LoginLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+function onClick () {
+  console.log('you clicked me')
+}
+
+export default defineComponent({
+  name: 'LoginLayout',
+  setup () {
+    return {
+      onClick
+    }
+  }
+})
+</script>
