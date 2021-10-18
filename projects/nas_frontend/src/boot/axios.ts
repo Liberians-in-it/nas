363fcf700e1ad528545a7db61bb13e5a9487bf0b
Jibao Mansaray <jibaomansaray@gmail.com>
2021-10-18 13:00:06 +0000

    Add helper methods

--- projects/nas_frontend/src/boot/axios.ts
+++ projects/nas_frontend/src/boot/axios.ts
@@ -1,5 +1,6 @@
 import { boot } from 'quasar/wrappers'
 import axios, { AxiosInstance } from 'axios'
+import { LocalStorage } from 'quasar'
 
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
@@ -13,29 +14,60 @@ declare module '@vue/runtime-core' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
+let api: AxiosInstance
+let hasCredential = false
 
-// get api host
-const hostElement = document.getElementsByName('api_host')
-let domain = ''
-if (hostElement) {
-  domain = (hostElement[0] as HTMLMetaElement).content
-}
-domain += 'api/v1'
-
-// get api token
-const apiTokenElement = document.getElementsByName('api_token')
-let apiToken = ''
-if (apiTokenElement) {
-  apiToken = (apiTokenElement[0] as HTMLMetaElement).content
-}
-
-const api = axios.create({
-  baseURL: domain,
-  headers: {
-    Accept: 'application/json',
-    Authorization: `Bearer ${apiToken}`
+const setupAxios = (refresh: boolean): AxiosInstance | undefined => {
+  if (api && !refresh) {
+    return api
   }
-})
+
+  // get api host from the template
+  const hostElement = document.getElementsByName('api_host')
+  let domain = ''
+  if (hostElement) {
+    domain = (hostElement[0] as HTMLMetaElement).content
+  }
+  domain += 'api/v1'
+
+  // get api token from the template
+  const apiTokenElement = document.getElementsByName('api_token')
+  let apiToken = ''
+  if (apiTokenElement) {
+    apiToken = (apiTokenElement[0] as HTMLMetaElement).content
+  }
+
+  // try getting host from local storage
+  if (!domain || domain.indexOf('{! api_host !}') === 0) {
+    domain = LocalStorage.getItem('api_host') || '{! api_host !}'
+  }
+
+  // try getting api token from local storage
+  if (!apiToken || apiToken.indexOf('{! api_token !}') === 0) {
+    domain = LocalStorage.getItem('api_token') || '{! api_token!}'
+  }
+
+  if (domain && domain.indexOf('{! api_host !}') !== 0 && apiToken && apiToken.indexOf('{! api_token !}') !== 0) {
+    hasCredential = true
+    api = axios.create({
+      baseURL: domain,
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${apiToken}`
+      }
+    })
+  } else {
+    hasCredential = false
+    api = axios.create()
+  }
+  return api
+}
+
+const axiosHasCredential = (): boolean => {
+  return hasCredential
+}
+
+setupAxios(false)
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
@@ -49,4 +81,8 @@ export default boot(({ app }) => {
   //       so you can easily perform requests against your app's API
 })
 
-export { api }
+export {
+  api,
+  setupAxios,
+  axiosHasCredential
+}
