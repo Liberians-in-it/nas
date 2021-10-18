916787b7b6be679f0fca3c9b03feab60b6f08924
Jibao Mansaray <jibaomansaray@gmail.com>
2021-10-18 13:01:55 +0000

    Register login route

--- projects/nas_frontend/src/router/routes.ts
+++ projects/nas_frontend/src/router/routes.ts
@@ -6,6 +6,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/Index.vue') }]
   },
+  {
+    path: '/login',
+    component: () => import('layouts/BlankLayout.vue'),
+    children: [{ path: '', component: () => import('pages/Login.vue'), name: 'login' }]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
