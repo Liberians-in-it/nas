6cacf162e0838fd089009f508bd334079c7c6548
Jibao Mansaray <jibaomansaray@gmail.com>
2021-10-18 13:01:11 +0000

    Redirect to the login page if the user is not login
    Remove unused code

--- projects/nas_frontend/src/pages/Index.vue
+++ projects/nas_frontend/src/pages/Index.vue
@@ -1,66 +1,23 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
-    <pre>
-      {{ result }}
-    </pre>
+    <h1>You are logged in!!!</h1>
   </q-page>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models'
-import ExampleComponent from 'components/CompositionComponent.vue'
-import { defineComponent, ref, reactive } from 'vue'
-import { client, UserType } from '../model/users'
+import { defineComponent } from 'vue'
+import { axiosHasCredential } from 'boot/axios'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
   name: 'PageIndex',
-  components: { ExampleComponent },
   setup () {
-    const result = reactive<{user: UserType | null }>({ user: null })
-
-    client.getMe()
-      .then((user: UserType) => {
-        result.user = user
-      }).catch(() => {
-        console.log("nothing here")
+    if (!axiosHasCredential()) {
+      void useRouter().replace({
+        name: 'login'
       })
-
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ])
-    const meta = ref<Meta>({
-      totalCount: 1200
-    })
-    return {
-      todos,
-      meta,
-      result
     }
+    return {}
   }
 })
 </script>
