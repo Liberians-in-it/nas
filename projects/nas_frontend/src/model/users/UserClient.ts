876745b444463d29a1e546b0f9085175da8072b9
Jibao Mansaray <jibaomansaray@gmail.com>
2021-10-18 11:51:58 +0000

    Remove null type

--- projects/nas_frontend/src/model/users/UserClient.ts
+++ projects/nas_frontend/src/model/users/UserClient.ts
@@ -2,14 +2,14 @@ import { api } from 'boot/axios'
 import { UserType } from './type'
 
 class UsersClient {
-  public getMe (): Promise<UserType | null> {
-    return new Promise<UserType | null>((resolve, reject) => {
+  public getMe (): Promise<UserType> {
+    return new Promise<UserType>((resolve, reject) => {
       api.get('user')
         .then((response) => {
           resolve(response.data as UserType)
         })
-        .catch(() => {
-          reject(null)
+        .catch((error) => {
+          reject(error)
         })
     })
   }
