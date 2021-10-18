1a973cda583d4baf2ae320048a109b62ef9b8423
Jibao Mansaray <jibaomansaray@gmail.com>
2021-10-18 12:57:22 +0000

    Add a blank layout

---
+++ projects/nas_frontend/src/layouts/BlankLayout.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <q-layout>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BlankLayout',
+
+  components: {
+  },
+
+  setup () {
+    return {
+    }
+  }
+})
+</script>
